8ee4a5d42d458a46e1c81efe53d1ce56ca579ba4
lucasisman <lucasitzmann@gmail.com>
2024-06-05 20:50:53 -0400

    modified:   package-lock.json modified:   package.json modified:   src/App.js modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = () => <h1>Bienvenida</h1>;
+
+const DisplayNumber = ({ match }) => {
+    const { number } = match.params;
+    return <h1>{number}</h1>;
+};
+
+const DisplayWord = ({ match }) => {
+    const { word } = match.params;
+    return <h1>{word}</h1>;
+};
+
+const StyledWord = ({ match }) => {
+    const { word, textColor, bgColor } = match.params;
+    const style = {
+        color: textColor,
+        backgroundColor: bgColor,
+    };
+    return <h1 style={style}>{word}</h1>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/:number" render={props => isNaN(props.match.params.number) ? <DisplayWord {...props} /> : <DisplayNumber {...props} />} />
+                <Route exact path="/:word/:textColor/:bgColor" component={StyledWord} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
